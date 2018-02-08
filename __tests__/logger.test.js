acb0d4391d08a0a3afbcebc97b18f521db72b52f
Alex Corre <alex@workpop.com>
2018-02-08 15:38:23 -0800

    fix(logger): prevent double when requestId is used

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -1,14 +1,35 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
 import Logger from '../src';
 
 const simpleLogging = new Logger('SIMPLE');
-
-const requestIdLogger = new Logger('SIMPLE', '234923423')
+const requestIdLogger = new Logger('SIMPLE', '234923423');
 
 describe('Logger', function () {
+
   describe('simple logging', function () {
+
+    let sandbox;
+
+    beforeEach(function () {
+      sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+    });
+
     it('logs errors', function () {
       simpleLogging.error('SUP');
       requestIdLogger.error('SUP');
     });
+
+    it('logs only one line when request ID is included', function () {
+      const logSpy = sandbox.spy(console, 'log');
+      requestIdLogger.info('log line text');
+      expect(logSpy.callCount).to.equal(1);
+    });
+
   });
+
 });
